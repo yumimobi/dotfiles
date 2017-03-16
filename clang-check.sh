454ec49112ecfba5a2d46b69dd1fa9b66f91af57
on99 <nanohugh@gmail.com>
2017-03-16 18:01:20 +0800

    Skip .//Examples/Pods when checking code format

--- clang-check.sh
+++ clang-check.sh
@@ -5,7 +5,7 @@ then
     find ./ -name "*.[hm]" -exec clang-format -i -style=file "{}" \;
 else
     echo "Checking Clang Format..."
-    find ./ -name "*.[hm]" -exec clang-format -style=file -output-replacements-xml "{}" \; | grep "<replacement " >/dev/null
+    find ./ -path .//Example/Pods -prune -o -name "*.[hm]" -exec clang-format -style=file -output-replacements-xml "{}" \; | grep "<replacement " >/dev/null
     if [ $? -ne 1 ]
     then
         echo "Commit did not match clang-format"
